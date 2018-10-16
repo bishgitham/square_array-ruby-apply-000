56ac0a117e3e14685332db8f71b985c5f8361664
bish <hamal.bishwash65@gmail.com>
2018-10-16 22:22:38 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
 new_array = []
 array.each do |x|
-square_no = x*x
+# square_no = x*x
 new_array.insert  (square_no)
 end
 end
 array = [9]
-# puts square_array(array)
+puts square_array(array)
